67d22492f49dca0c5ae7a9dc821396a9402749ee
BUSOLA12 <iyiolaolubusola@gmail.com>
2022-09-08 13:03:52 -0700

    4.puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inlude<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  puts("\"programming is like building a muitilingual puzzle");
+  return (0)
+    }
