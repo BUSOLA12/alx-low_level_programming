b6faf45743d861bcb3404841fb016cb1d094f2be
BUSOLA12 <iyiolaolubusola@gmail.com>
2022-09-21 17:19:44 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,7 +7,7 @@
 void print_number(int n)
 {
 unsigned int m, d, count;
-if (n < 0)
+if(n < 0)
 {
 _putchar(45);
 m = n * -1;
@@ -18,12 +18,12 @@ m = n;
 }
 d = m;
 count = 1;
-while (d > 9)
+while(d > 9)
 {
 d /= 10;
 count *= 10;
 }
-for (; count >= 1; count /= 10)
+for(; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
 }
