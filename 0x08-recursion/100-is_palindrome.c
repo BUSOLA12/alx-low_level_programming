66d78648fc4b2cea044a4c7231e1699cf7969628
BUSOLA12 <iyiolaolubusola@gmail.com>
2022-09-29 15:48:41 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,10 +23,10 @@ int comparator(char *s, int n1, int n2)
 if (*(s + n1) == *(s + n2))
 {
 if (n1 == n2 || n1 == n2 + 1)
-return(1);
+return (1);
 return (0 + comparator(s, n1 + 1, n2 - 1));
 }
-return(0);
+return (0);
 }
 /**
  * is_palindrome - detects if a string is a palindrome.
