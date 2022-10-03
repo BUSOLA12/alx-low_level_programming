d949f07c7d44aaafdcf9f2b28594a4c51da15f55
BUSOLA12 <iyiolaolubusola@gmail.com>
2022-10-03 12:36:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,10 @@ int main(int argc, char *argv[])
 {
 int num_1, num_2, mul;
 if (argc != 3)
-printf("Error\n");
-else
 {
-num_1 = atoi(argv[1]);
-num_2 = atoi(argv[2])
-printf("%d\n", mul);
+printf("Error\n");
+return (1);
 }
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
