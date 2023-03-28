94727515f7ab2df81147de817acad72c767215c1
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-28 12:54:33 -0700

    wrote a code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
