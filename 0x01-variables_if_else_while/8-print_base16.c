31fae6b941d9d9dbb72d9795dadba60ba602e989
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-28 15:19:08 -0700

    wrote a code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (j = 97; j < 103; i++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
