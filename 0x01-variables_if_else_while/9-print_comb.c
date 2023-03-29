0a19d6978cda252a025e4dc2e312459ac35b9a6e
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 06:02:57 -0700

    wrote a code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#inlclude <stdio.h>
+/**
+ * main - prints single digits from 0-9 and seperates them
+ * with comma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
