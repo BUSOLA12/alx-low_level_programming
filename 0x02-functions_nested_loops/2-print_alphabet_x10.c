8f7d66df59b3a7d94f92ad1a32c0edddb00fa5ba
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 15:06:53 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints ten times th alphabet in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i, A;
+for (i = 0; i < 10; i++)
+{
+for (A = 97; A < 123; A++)
+{
+_putchar(A);
+}
+}
+_putchar('\n'');
+}
