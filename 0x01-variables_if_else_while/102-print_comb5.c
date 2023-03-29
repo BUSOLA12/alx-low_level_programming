916d8440917c1f8dd5bdeebe4feb4d59fc6823f3
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 11:01:34 -0700

    wrote a code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main- prints four digits
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
-    int i, j, k, l;
-    for (i = 48; i < 58; i++)
-    {
-        for (j = 48 ; j < 57; j++)
-        {
-            for (k = 48; k < 58; k++)
-            {
-                for (l = 49 ; l < 58; l++)
-                {
-
-               putchar(i);
-            putchar(j);
-            putchar(' ');
-            putchar(k);
-            putchar(l);
-            if (i == 57 && j == 57 && k == 56 && l == 57)
-                continue;
-            putchar(',');
-            putchar(' ');
-            }
-
-        }
-    }
-    }
-    putchar('\n');
-    return 0;
+int i, j, k, l;
+for (i = 48; i < 58; i++)
+{
+for (j = 48 ; j < 57; j++)
+{
+for (k = 48; k < 58; k++)
+{
+for (l = 49 ; l < 58; l++)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(k);
+putchar(l);
+if (i == 57 && j == 57 && k == 56 && l == 57)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return 0;
 }
 
