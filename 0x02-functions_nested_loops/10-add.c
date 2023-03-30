246cdc3ce4c2d6146626ad4bcfeac1e56ada9e55
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-30 14:58:43 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - it adds to numbers
+ *@i: number 1
+ *@j: number 2
+ * Return: Always result (Success)
+ */
+int add(int i, int j)
+{
+result = i + j;
+return (result);
+
+}
