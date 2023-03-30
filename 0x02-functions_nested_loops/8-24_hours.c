0bd0b5390b42115b152883c7183c05423607f65d
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-30 10:03:30 -0700

    wrote a code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,12 +7,14 @@ void jack_bauer(void)
 int i, j, k, l;
 for (i = 48; i < 51; i++)
 {
-for (j = 48; j < 52; j++)
+for (j = 48; j < 58; j++)
 {
 for (k = 48; k < 54; k++)
 {
 for (l = 48; l < 58; l++)
 {
+if (i <= 50 && j <= 51)
+break;
 _putchar(i);
 _putchar(j);
 _putchar(':');
