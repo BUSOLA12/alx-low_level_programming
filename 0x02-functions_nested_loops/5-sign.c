dd6545718c58fc49fe4800434cc485530dc41529
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 17:45:26 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of characters
+ * @n: character to be checked
+ * Return: Always 1 (Success), 0 if zero, -1 otherwise
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
+_putchar('\n');
+}
