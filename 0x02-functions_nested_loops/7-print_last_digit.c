b066bb5d056451883387c9bc56df53cb4687928d
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 18:14:18 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ *@n: variable to be checked
+ *Return: Always -l if l is negative, l if otherwise
+ */
+int print_last_digit(int n)
+{
+int l = n % 10;
+if (l < 0)
+{
+return (-l);
+}
+else
+{
+return (l);
+}
+}
+
