48b7b89dfc0bf9c37e6557156f2729c33332ae15
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-29 17:59:36 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - finds the absolute value of a number
+ *@n: this is the variable checked
+ *Return: Always -n (Success), n if otherwise
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
