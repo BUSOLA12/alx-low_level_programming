42624e6e7d2ecbdc43ea0f7dd0164c78a2200e1e
BUSOLA12 <iyiolaolubusola@gmail.com>
2023-03-31 08:28:38 -0700

    wrote a code

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table - prints the time table from 0 - 15
+ * @n: receives the argument
+ */
+void print_times_table(int n)
+{
+int a, b, op, n;
+if (n >=0 && n <= 15)
+{
+for (a = 0; a <= n ; a++)
+{
+for (b = 0; b <= n; b++)
+{
+op = a * b;
+if (op < 10)
+{
+_putchar(op + '0');
+if (b != n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+if (op > 9 && op < 100)
+{
+_putchar(op / 10 + '0');
+_putchar(op % 10 + '0');
+if (b != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+if (op > 99)
+{
+_putchar(((op / 100) % 10) + '0');
+_putchar(((op / 10) % 10) + '0');
+_putchar(op % 10 + '0');
+if (b != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+return 0;
+}
+}
+
+
