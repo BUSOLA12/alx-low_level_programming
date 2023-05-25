d0bb78d26004a3f7a902647dab182ebcdafeaf15
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-25 12:45:05 -0700

    made correction

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+int limit = 1024;
+int sum_of_multiples = 0;
+int i;
+for (i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum_of_multiples += i;
+}
+}
+printf("sum of the multiples is equal to: %d", sum_of_multiples);
+return(0);
+}
