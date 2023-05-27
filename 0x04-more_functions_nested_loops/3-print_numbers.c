0e448a14d86921e6ba05922f92160395d0dea353
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-27 10:49:36 -0700

    wrote a code

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+printf("%d\n", i);
+}
+}
+
