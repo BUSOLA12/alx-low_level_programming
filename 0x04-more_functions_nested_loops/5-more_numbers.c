f24ca6dd411454a0502469f4d5a6119efed78416
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-27 14:38:41 -0700

    wrote a code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,8 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 15; j++)
 {
 if (j > 9)
-_putchar((i / 10) + 48);
-_putchar((i % 10) + 48);
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 }
 _putchar('\n');
 }
