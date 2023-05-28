cab0757537e0514fc9bdd7c74e95df5b75d0fdb3
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-28 03:50:37 -0700

    wrote a code

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+ * print_square - print a square
+ * @size: size of square
+ * Description: Can only use _putchar to print. Use '#' to print square.
+ */
+void print_square(int size)
+{
+int i, j;
+for (i = 1; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+_putchar('#')
+}
+_putchar('\n');
+}
+}
