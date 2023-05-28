936d2a9747bd30583907f2a94441e6611298cbcb
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-28 14:59:50 -0700

    wrote a code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,17 +6,17 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if((i % 3 == 0) && (i % 5 == 0))
 {
 printf(" FizzBuzz");
 }
-else if (n % 3 == 0)
+else if (i % 3 == 0)
 {
 printf(" Fizz");
 }
-else if (n % 5 == 0)
+else if (i % 5 == 0)
 {
 printf(" Buzz");
 }
