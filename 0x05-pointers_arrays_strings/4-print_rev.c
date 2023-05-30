f6b2cf4df0db7f246370cc25c4acd5ac1c34b531
Iyiola Olubusola <iyiolaolubusola@gmail.com>
2023-05-29 18:53:18 -0700

    wrote a code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
- * rev_string - Reverses a string.
- * @s: The string to be reversed.
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
  */
 void print_rev(char *s)
 {
