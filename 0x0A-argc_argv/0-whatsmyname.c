9020f743ea2b4507e4230ec0778dc2c3e101a914
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-09 03:51:05 -0700

    wrote a code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -6,8 +7,8 @@
  *
  * return: Always 0 (Success)
  */
-int main (int argc __attribute__ ((unused)), char **argv)
+int main (int argc __attribute__ ((unused)), char *argv[])
 {
-printf("%s\n", *argv[0]);
+printf("%s\n", *argv);
 return(0);
 }
