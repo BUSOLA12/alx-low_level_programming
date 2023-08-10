8f20a9bead1ccf56eb7687339c84f4facd28a06f
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-10 09:24:58 -0700

    wrote a code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,53 +1,75 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * _atoi - converts a string to an integer
- * @str: string to be converted
+ * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-int _atoi(char *str)
+int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
-while (*str == ' ' || *str == '\t')
-{
-str++;
-}
-if (*str == '-')
-{
-sign = -1;
-str++;
-}
-else if (*str == '+')
-{
-str++;
-}
-while (*str >= '0' && *str <= '9')
-{
-int digit = *str - '0';
-result = result * 10 + digit;
-str++;
-}
-return ((result) * (sign));
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
 /**
- * main- calculates multiplication of two numbers
+ * main - multiplies two numbers
  * @argc: number of arguments
- * @argv: array of arguments passed to main
+ * @argv: array of arguments
  *
- * Return: 1 (argc = 0) 0 (Success)
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-if (argc == 0)
-{
-char *str[] = "Error";
-printf("%s\n", str);
-return (1);
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
-int result = (_atoi(argv[0])) * (_atoi(argv[1]));
-print("%d\n", result);
-return (0);
-}
+
+
