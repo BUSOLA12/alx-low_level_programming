b88cf4da1d04b7f829a9e8c26c27a2e69882880b
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-10 01:56:53 -0700

    wrote a code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints arguments passed to it
+ * @argc: number of arguments
+ * @argv: arguments passed to main
+ * @count: iteration
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int count;
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", *argv[count]);
+}
+return (0);
+}
