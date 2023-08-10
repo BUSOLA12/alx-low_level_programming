80969672c60fe13fd0ed4b15e9b15d315a86f21f
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-10 02:40:53 -0700

    wrote a code

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- calculates multiplication of two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments passed to main
+ *
+ * Return: 1 (argc = 0)
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+if (argc = 0)
+{
+printf("%s\n", "Error");
+return (1);
+}
+int result = argv[0] * argv[1];
+print("%d\n", result);
+return (0);
+}
