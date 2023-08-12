09f307838164293890649911c56bd6752fb5a1ad
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-12 02:34:37 -0700

    wrote a code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,23 +11,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
-int i = 0;
-if (size == 0)
-{
-	return (NULL);
-}
-char *a;
-a = (char *) malloc(sizeof(char) * size);
-if (array == NULL)
-{
-	return (NULL);
-}
-while (i < size)
-{
-*(a + i) = c;
-i++;
-}
-*(a + i + 1) = '\0'
-return (a);
+	char *p;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	p = (char *) malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (0);
+
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+
+	*(p + i) = '\0';
+
+	return (p);
 }
 
