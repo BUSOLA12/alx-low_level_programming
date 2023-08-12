f02b56d2572da7347cb788d75fcbf975eb2c51f1
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-12 08:34:18 -0700

    wrote a code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,13 @@ char *str_concat(char *s1, char *s2)
 	size_t len1;
 	size_t len2;
 	char *concat_str;
-if (s1 == NULL && s2 == NULL)
+if (s1 == NULL)
 {
-
-	s1 = " ";
-	s2 = " ";
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
 }
 len1 = strlen(s1);
 len2 = strlen(s2);
