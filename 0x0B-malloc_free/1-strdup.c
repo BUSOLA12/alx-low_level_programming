67e15957612653a8ea49485c9b3ce3da8cb02130
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-12 05:44:18 -0700

    wrote a code

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to the string copied from the parameter
+ * @str: string to be copied
+ * Return: NULL (fail) pointer to copied string (Success)
+ */
+char *_strdup(char *str)
+{
+if (str == NULL)
+{
+return (NULL);
+}
+size_t len = strlen(str);
+char *new_str = (char *) malloc(sizeof(char) * len;)
+if (new_str == NULL)
+{
+return (NULL);
+}
+str[len + 1] = new_str[len + 1];
+str[len + 1] = '\0';
+return new_str[len + 1];
+}
