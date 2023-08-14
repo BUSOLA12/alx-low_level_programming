002d1d82dad34478c25e4cd3f5f0037cc2da82ba
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-14 16:59:56 -0700

    wrote a code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dog's owner name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
