2c4bcbd64104fc349257ebaeaeaca2537e2edc11
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-14 19:04:50 -0700

    wrote a code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,30 +60,32 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
-int len1, len2;
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-{
-	return (NULL);
-}
-size_t len1 = _strlen(name);
-size_t len2 = _strlen(owner);
-dog -> name = malloc(sizeof(char) * (len1 + 1));
-if (dog -> name == NULL)
-{
-	free(dog);
-	return (NULL);
-}
-dog -> owner = malloc(sizeof(char) * (len2 + 1));
-if (dog -> owner == NULL)
-{
-	free(dog);
-	free(d -> name);
-	return (NULL);
-}
-_strcpy(dog -> name, name);
-_strcpy(dog -> owner, owner);
-dog -> age = age;
-return (dog);
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
