4efbca7caf43998019b4e108986366cd16a631e4
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-16 16:45:27 -0700

    wrote a code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t j;
-	if (array && action)
+	if (!array || !action)
 	{
+		return;
+	}
 		for (j = 0; j < size; j++)
 		{
 			action(array[j]);
 		}
-	}
 }
