a25c0eb8b3234296197039e9f9413e45661ec850
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-16 17:19:08 -0700

    wrote a code that finds integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers"
+/**
+ * int_index - checks for an integer
+ * @array: array to iterate
+ * @size: size of array
+ * @cmp: pointer to function that checks for integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i, result;
+	if (!array || !cmp)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for(i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+		{
+			return (i);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
