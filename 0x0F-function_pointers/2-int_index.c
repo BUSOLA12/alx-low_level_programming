f5936c7082ca74dc134d0129781e6025e0a74084
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-16 17:24:17 -0700

    wrote a code that finds integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,6 +4,8 @@
  * @array: array to iterate
  * @size: size of array
  * @cmp: pointer to function that checks for integer
+ * Return: index of the first element for which the cmp function does not return
+ * 0, or -1 if no match is found
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
