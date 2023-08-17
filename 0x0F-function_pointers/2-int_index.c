67c0957ad800a55d2dba4fc80022576f37bc3b99
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-16 17:32:22 -0700

    wrote a code that finds integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i, result;
+	int i, result;
 	if (!array || !cmp)
 	{
 		return (-1);
@@ -25,8 +25,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			return (i);
 		}
-		else
-		{
-			return (-1);
-		}
 	}
+	return (-1);
+}
