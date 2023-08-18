fdf0ec2fcace44d352c62a2828de21cb2b5a3b30
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-18 07:23:36 -0700

    wrote a code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,27 +9,26 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
-unsigned int i;
-char *str;
+	va_list valist;
+	unsigned int i;
+	char *str;
 
-va_start(valist, n);
-for (i = 0; i < n; i++)
-{
-	str = va_arg(valist, char *);
-	if (str)
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
 	{
-	printf("%s", str);
+		str = va_arg(valist, char *);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
-	else
-	{
-	printf("nil");
-	}
-	if (separator && i < n - 1)
-	{
-	printf("%s", separator);
-	}
-}
-va_end(valist);
-printf("\n");
+
+	printf("\n");
+	va_end(valist);
 }
