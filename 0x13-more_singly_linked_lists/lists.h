abff775c6ff58d24628f01554edaa11803a2416c
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-29 14:09:19 -0700

    wrote a code

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - type defintion for singly link list
+ * @n: integer in each node
+ * @next: pointer to the next node
+ * Description: definition for singly link list
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_s;
+
+size_t print_listint(const listint_t *h);
