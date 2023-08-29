300732017b52fa9cfe830eae91e3ec8b50856ea5
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-29 14:29:53 -0700

    wrote a code

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * listint_len - returns the number of elements in
  * a linked list.
@@ -8,12 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		h = h ->next;
-		count++;
+		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
