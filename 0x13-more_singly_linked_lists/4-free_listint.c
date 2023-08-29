38e92e5899515dddcf81e4dbe45c43aef70274fa
iyiola olubusola <iyiolaolubusola@gmail.com>
2023-08-29 15:27:50 -0700

    wrote a code

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * free_listint - frees a linked list
  * @head: head of a list.
@@ -8,6 +9,7 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
+
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
